923b480f07fd294cea9c44afe1c48c543b5ef632
shmuel <shmuel.friedman@hp.com>
2019-09-06 00:34:18 +0300

    added moduleInjector: for loading modules from js-console/snippets. added scrapping-helper

---
+++ src/power-moduleInjector.js
@@ -0,0 +1,71 @@
+(function() {
+    var jspower = window.jspower = window.jspower || {};
+    if (jspower.injector) return;
+    jspower.injector = {fetchSync,SyncPromise};
+
+    jspower.injector.importSync = url => fetchSync(url)
+        //.then(response => response.text())
+        .then(text => eval(text),/*)
+        .catch(*/r => console.error(r)); 
+
+    function fetchSync(url) {
+        return new SyncPromise((res,rej)=>{
+            let resp;
+            var oReq = new XMLHttpRequest();
+            //oReq.addEventListener("load", e => res(oReq.responseText));
+            oReq.addEventListener("error", e => rej(oReq.responseText));
+            oReq.open("GET", url, false);
+            oReq.send();
+            if (oReq.responseText)
+                res(oReq.responseText);
+        });
+    }
+    function SyncPromise(f) {
+        let that = this;
+        let _cb;
+        let _rejcb;
+        let resolved;
+        let rejected;
+        this.then = function(cb,rejcb) {
+            _cb = cb;
+            _rejcb = rejcb;
+            if (resolved !== null && resolved != undefined)
+                return Promise.resolve(_cb(resolved)||resolved);
+            else if (rejected !== null && rejected != undefined)
+                return Promise.reject(_rejcb(rejected)||rejected);
+            else
+                return Promise.resolve(that);
+        };
+        let res = (v) => {
+            if ((resolved !== null && resolved != undefined) || (rejected !== null && rejected != undefined)) return;
+            resolved = v !== null && v != undefined ? v : true;
+            if (_cb) _cb(v);
+        };
+        let rej = (v) => {
+            if ((resolved !== null && resolved != undefined) || (rejected !== null && rejected != undefined)) return;
+            rejected = v !== null && v != undefined ? v : true;
+            if (_rejcb) _rejcb(v);
+        };
+        /*let ret = */
+        f(res, rej);
+        return that;
+    }
+
+    jspower.injector.importCors = url => fetch(url)
+        .then(response => response.text())
+        .then(text => eval(text))
+        .catch(r => console.error(r));
+
+    //"<meta http-equiv=\"Content-Security-Policy\" content=\"default-src *; style-src 'self' http://* 'unsafe-inline'; script-src 'self' http://* 'unsafe-inline' 'unsafe-eval'\" />"
+    /*(function(e, s) {
+        e.src = s;
+        e.onload = function() {
+            window.$$3 = jQuery.noConflict();
+            console.log('jQuery injected');
+        };
+        document.head.appendChild(e);
+    })(document.createElement('script'), );*/
+
+    //jspower.injector.importSync(jqueryPath).then(() => window.$$3 = jQuery.noConflict());
+    //jspower.injector.importSync(lodashUrl);
+})();
---
+++ src/power-scrapping.js
@@ -0,0 +1,85 @@
+(function() {
+    var jspower = window.jspower = window.jspower || {};
+    if (jspower.scrapper) return;
+    jspower.scrapper = {};
+
+    let $_$ = document.querySelectorAll.bind(document);
+    let $_1 = document.querySelector.bind(document);
+
+    Object.assign(jspower.scrapper, {docReady, waitForDomQuery, popNav});
+    Object.assign(window, {$_$,$_1});
+    
+    jspower.scrapper.searchInDom = (phrase) => {
+        const words = phrase.split(' ');
+        let query = words.map(w => `span.node_text:contains(${w})`).join(',');
+        return $_$(query); //$$3
+    };
+    
+    function docReady(doc) {
+      if (!doc) doc = document;
+      return new Promise((res,rej) => {
+          if (doc.readyState === "complete")
+            res();
+          else {
+            let _res = (e)=> {
+                doc.removeEventListener('DOMContentLoaded', _res);
+                res(e);
+            };
+            doc.addEventListener('DOMContentLoaded', _res, false);
+          }
+      });
+    }
+    
+    async function waitForDomQuery(qry, doc, win, waitForJquery) {
+      if (!doc) doc = document;
+      if (!win) win = window;
+      let $_1;
+      if (waitForJquery && jspower.waitObjectProp)
+        $_1 = await new Promise((res,rej)=>{
+            try {
+                jspower.waitObjectProp(win, '$', res);
+            }
+            catch(ex) { rej(ex); }
+        });
+      else
+        $_1 = win.$.bind(win) || doc.querySelector.bind(doc);
+      let elem = $_1(qry);
+      if (elem)
+          return elem;
+      else {
+          await taskDelay(10);
+          return await waitForDomQuery(qry);
+      }
+    }
+    
+    let win;
+    async function popNav(url, domQuery, waitForJquery) {
+        if (!win)
+            win = launch(url);
+        else
+            win.location.href = url;
+        await taskDelay(10);
+        await docReady(win.document);
+        if (domQuery) 
+            return [
+                win, 
+                await waitForDomQuery(domQuery, win.document, win, waitForJquery)
+            ];
+        return win;
+    }
+    async function taskDelay(millis) {
+        await new Promise((rslv,rjct) => {
+            setTimeout(rslv, millis);
+        });
+    }
+    function launch(url) {
+      var win = window.open(url, '_blank');
+      win.focus();
+      return win;
+    }
+
+    // var jqueryPath = 'http://code.jquery.com/jquery-latest.min.js';
+    // var lodashUrl = 'https://cdn.jsdelivr.net/npm/lodash@4.17.15/lodash.min.js';
+    //jspower.injector.importSync(jqueryPath).then(() => jspower.injector.$$3 = jQuery.noConflict());
+    //jspower.injector.importSync(lodashUrl);
+})();
